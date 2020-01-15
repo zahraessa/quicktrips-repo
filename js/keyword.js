ce4a69568629f975386eb5bb0da4f7d3f38da8ea
张婧怡 <zhangjingyi@zhangjingyideMacBook-Pro.local>
2020-01-15 21:04:12 +0000

    limit of 5 and checkbox added

--- js/keyword.js
+++ js/keyword.js
@@ -1,9 +1,10 @@
-$(function () {
-    $('#search').on('keyup', function () {
-        var pattern = $(this).val();
-        $('.items-collection .items').hide();
-        $('.items-collection .items').filter(function () {
-            return $(this).text().match(new RegExp(pattern, 'i'));
-        }).show();
-    });
+$('input[type=checkbox]').on('change', function (e) {
+    if ($('input[type=checkbox]:checked').length > 5) {
+        $(this).prop('checked', false);
+        alert("Sorry. Only 5 keyword is allowed.");
+    }
+});
+
+$('checkbox').children('input[type=checkbox]').click(function (e) {
+    $(this).children().css('opacity','1');
 });
