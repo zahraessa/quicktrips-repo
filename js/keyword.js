4f66a5dc48c619c4d48caa66d237faf7fc7eaecf
张婧怡 <zhangjingyi@zhangjingyideMacBook-Pro.local>
2020-01-15 12:46:00 +0000

    only one could be chosen

---
+++ js/keyword.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('#search').on('keyup', function () {
+        var pattern = $(this).val();
+        $('.items-collection .items').hide();
+        $('.items-collection .items').filter(function () {
+            return $(this).text().match(new RegExp(pattern, 'i'));
+        }).show();
+    });
+});
