a442931ede18452e36fbe4bc740fa329200b858d
zahraessa99 <zcabzes@ucl.ac.uk>
2020-01-23 14:44:53 +0000

    extra api key

--- app/gethotels.py
+++ app/gethotels.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def getCityInfo(city):
     url = "https://tripadvisor1.p.rapidapi.com/locations/search"
 
@@ -17,7 +18,7 @@ def getCityInfo(city):
     except:
         headers = {
             'x-rapidapi-host': "tripadvisor1.p.rapidapi.com",
-            "x-rapidapi-key":  "1f79334269mshfc4b60a19d501cep1244f7jsnaf9b5cb281bb"
+            "x-rapidapi-key": "1f79334269mshfc4b60a19d501cep1244f7jsnaf9b5cb281bb"
         }
         response = requests.request("GET", url, headers=headers, params=querystring)
         return response
@@ -26,10 +27,11 @@ def getCityInfo(city):
 def getCityId(city):
     return getCityInfo(city).json()['data'][0]['result_object']["location_id"]
 
-def getCityDescription (city):
+
+def getCityDescription(city):
     return getCityInfo(city).json()['data'][0]['result_object']["geo_description"]
 
-
+#the api call limit is 500 calls so I've added extra API keys as a fail safe until we decide on an alternative
 
 def getHotelDetails(city):
     cityid = getCityId(city)
@@ -48,18 +50,27 @@ def getHotelDetails(city):
         response.json()['data'][0]['photo']['images']['large']['url']
         return response
     except:
-        headers = {
-            'x-rapidapi-host': "tripadvisor1.p.rapidapi.com",
-            "x-rapidapi-key":  "1f79334269mshfc4b60a19d501cep1244f7jsnaf9b5cb281bb"
-        }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        return response
+        try:
+            headers = {
+                'x-rapidapi-host': "tripadvisor1.p.rapidapi.com",
+                "x-rapidapi-key": "1f79334269mshfc4b60a19d501cep1244f7jsnaf9b5cb281bb"
+            }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            return response
+        except:
+            headers = {
+                'x-rapidapi-host': "tripadvisor1.p.rapidapi.com",
+                "x-rapidapi-key": "972ab58325mshaf4cbe07802218fp1fcb76jsn1e3664871862"
+            }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            return response
+
 
 
 def getHotelName(city, i):
     return getHotelDetails(city).json()['data'][i]['name']
 
+
 def getHotelPhoto(city, i):
     photo = getHotelDetails(city).json()['data'][i]['photo']['images']['large']['url']
     return photo
-
