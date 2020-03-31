97a5cf5d2a926de5d1c1cd56fee48d87d47cd71f
zhangjingyi <ZMSW@163.com>
2020-03-31 23:06:09 +0800

    color with rating of userpage and css of footer

--- app/static/html5-global-map/js/index.js
+++ app/static/html5-global-map/js/index.js
@@ -1,24 +1,43 @@
-let list = getParamValue("list");
-filledList(list);
+let countryList = getParamValue(0);
+let ratingList = getParamValue(1);
+filledList(countryList, ratingList);
 
-function getParamValue()
+function getParamValue(n)
 {
+    // n = 0 get the country while n = 1 get the rate
     var url = window.location.search.substring(1); //get rid of "?" in querystring
-    var pArr = url.split('='); //split key and value
+    var qArray = url.split('&'); //get key-value pairs
+    var pArr = qArray[n].split('='); //split key and value
     var param = pArr[1].split("%2C");
-    return param;
+    return param
+
 }
 
-function filledList(list) {
-    for (let i = 0; i < list.length; i++) {
-        filled(list[i]);
+function filledList(countryList, ratingList) {
+    for (let i = 0; i < countryList.length; i++) {
+        filled(countryList[i], ratingList[i]);
     }
 }
 
-function filled(country) {
+function filled(country, rating) {
     let x = document.getElementById(country);
-    let a = x.getAttribute("fill");
-    x.setAttribute("fill", "#477cb2");
-    let b = x.getAttribute("fill");
+    if (rating === "0") {
+        x.setAttribute("fill", "rgba(89,184,255,0.59)");
+    }
+    else if (rating === "1") {
+        x.setAttribute("fill", "rgba(83,173,238,0.7)");
+    }
+    else if (rating === "2") {
+        x.setAttribute("fill", "rgba(81,160,223,0.75)");
+    }
+    else if (rating === "3") {
+        x.setAttribute("fill", "rgba(78,148,208,0.8)");
+    }
+    else if (rating === "4") {
+        x.setAttribute("fill", "rgba(63,126,192,0.9)");
+    }
+    else if (rating === "5") {
+        x.setAttribute("fill", "#477cb2");
+    }
 }
 
